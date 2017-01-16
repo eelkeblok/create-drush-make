95469eab1b54e571726c82c7941f7ac5c7e80849
Eelke Blok <eelke@blokspeed.net>
2017-01-16 11:40:31 +0100

    Refactor the actual writing in separate functions

--- create-drush-make.php
+++ create-drush-make.php
@@ -1,34 +1,32 @@
 <?php
-wl('core = 7.x');
-wl('defaults[projects][subdir] = contrib');
-wl();
-wl('api = 2');
-wl();
+// See if we should be generating legacy format or yml format.
+$format = 'legacy';
+$patch_path = 'patches';
+$docroot = 'docroot';
 
-$p = 'projects[drupal]';
-wl($p . '[type] = "core"');
-wl($p . '[subdir] = ""');
-wl($p . '[directory_name] = ""');
+foreach ($argv as $value) {
+  if (strpos($value, 'yml') !== FALSE) {
+    $format = 'yml';
+  }
+}
 
+writePreface();
 
 // Determine Drupal core version.
-$bootstrap_inc = file_get_contents('includes/bootstrap.inc');
+$bootstrap_inc = file_get_contents($docroot . '/includes/bootstrap.inc');
 $matches = array();
 
 preg_match("/define\\('VERSION', '(\\d.\\d\\d)'\\);/", $bootstrap_inc, $matches);
 
-wl($p . '[version] = "' . $matches[1] . '"');
-
 // Find patches in the patches directory for core.
-find_patches('drupal', 'patches/core');
+$patches = findPatches('drupal', $patch_path . '/core');
 
-// Blank line after the core entry.
-wl();
+writeCore($matches[1], $patches);
 
 // Find all contrib modules and their versions and patches. Note that we are
 // assuming directory names == project names (which not necessarily equals
 // module names).
-$contrib_dir = 'sites/all/modules/contrib';
+$contrib_dir = $docroot . '/sites/all/modules/contrib';
 $modules = scandir($contrib_dir);
 
 foreach ($modules as $module) {
@@ -43,15 +41,11 @@ foreach ($modules as $module) {
         if (preg_match('/version = "?7\.x-(\d.\d{1,2}(-[a-zA-Z0-9]*)?)"?/', $info, $matches)) {
           // If we found a version string, build the project entry.
           $version = $matches[1];
-          $p = 'projects[' . $module . ']';
-          wl($p . '[version]  = "' . $version . '"');
 
           // See if we have any patches for this module.
-          find_patches($module, 'patches/contrib/' . $module);
+          $patches = findPatches($module, 'patches/contrib/' . $module);
 
-          // Blank line at the end of the entry.
-          wl();
-
+          writeModule($module, $version, $patches);
           // Only process a single .info file per directory. We'll assume each
           // one contains the same version.
           break;
@@ -81,16 +75,55 @@ function wl($string = '') {
  *    directory in the flat project.
  *
  */
-function find_patches($project, $path) {
+function findPatches($project, $path) {
   // Find patches in the patches directory for core.
   $dir = 'sites/all/' . $path;
+  $files = [];
+
   if (is_dir($dir)) {
     $files = scandir($dir);
+  }
 
-    foreach ($files as $file) {
-      if (strpos($file, '.patch')) {
-        wl('projects[' . $project . '][patch][] = "' . $path . '/' . $file . '"');
-      }
+  return $files;
+}
+
+function writePreface() {
+  wl('core = 7.x');
+  wl('defaults[projects][subdir] = contrib');
+  wl();
+  wl('api = 2');
+  wl();
+}
+
+function writeCore($version, $patches) {
+  global $patch_path;
+
+  $p = 'projects[drupal]';
+  wl($p . '[type] = "core"');
+  wl($p . '[subdir] = ""');
+  wl($p . '[directory_name] = ""');
+  wl($p . '[version] = "' . $version . '"');
+
+  writePatches($patches, 'drupal', $patch_path . '/core');
+
+  // Blank line after the core entry.
+  wl();
+}
+
+function writePatches($patches, $project, $path) {
+  foreach ($patches as $file) {
+    if (strpos($file, '.patch')) {
+      wl('projects[' . $project . '][patch][] = "' . $path . '/' . $file . '"');
     }
   }
+}
+
+function writeModule($module, $version, $patches) {
+  $p = 'projects[' . $module . ']';
+  wl($p . '[version]  = "' . $version . '"');
+
+  writePatches($patches, $module, 'patches/contrib/' . $module);
+
+  // Blank line at the end of the entry.
+  wl();
 }
